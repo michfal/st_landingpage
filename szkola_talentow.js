d87130b55f1cfff530c7f112a544ed96949088ac
michfal <michfal86@gmail.com>
2018-10-11 19:39:37 +0200

    hiding navbar
    hiding navbar, smooth page scrolling, linking icons to sections

--- szkola_talentow.js
+++ szkola_talentow.js
@@ -1,18 +1,20 @@
+
+/*----------------------------*\
+#smooth scroll
+\*----------------------------*/
+
 function smoothScroll(target, duration) {
     var target = document.querySelector(target);
     var targetPosition = target.getBoundingClientRect().top;
     var startPosition = window.pageYOffset;
     var startTime = null;
 
-    
-    
     function animation(currentTime) {
         if(startTime === null) startTime = currentTime;
         var timeElapsed = currentTime - startTime;
         var run = ease(timeElapsed, startPosition, targetPosition, duration);
         window.scrollTo(0, run);
         if(timeElapsed < duration) requestAnimationFrame(animation);
-    
     }
 
     function ease(t, b, c, d) {
@@ -31,3 +33,26 @@ var opracBierzMat = document.querySelector('#obm--button');
 //     console.log("clicked")
 //     smoothScroll('#oprac--bierz--mat', 1000);
 // });
+
+/*----------------------------*\
+#smooth scroll
+\*----------------------------*/
+
+var navbar = document.querySelector('.navbar');
+
+var topImage = document.querySelector('.top_image');
+
+
+
+
+window.addEventListener('scroll', function () {
+    var scrollYdist = window.pageYOffset;
+    var topHeight = topImage.clientHeight;
+    console.log(scrollYdist)
+    if (topHeight < scrollYdist) {
+        navbar.style.top = '0px';
+    }
+    else{
+        navbar.style.top = '-100px';
+    }    
+})
